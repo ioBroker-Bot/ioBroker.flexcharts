2bb849ea0821dfe5d81141cf8848fdecec6c17d4
ioBroker Adapter Creator <noreply@iobroker.net>
2024-05-25 16:47:12 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,103 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'flexcharts adapter settings': {
+		'en': 'Adapter settings for flexcharts',
+		'de': 'Adaptereinstellungen für flexcharts',
+		'ru': 'Настройки адаптера для flexcharts',
+		'pt': 'Configurações do adaptador para flexcharts',
+		'nl': 'Adapterinstellingen voor flexcharts',
+		'fr': "Paramètres d'adaptateur pour flexcharts",
+		'it': "Impostazioni dell'adattatore per flexcharts",
+		'es': 'Ajustes del adaptador para flexcharts',
+		'pl': 'Ustawienia adaptera dla flexcharts',
+		'zh-cn': 'flexcharts的适配器设置'
+	},
+	'port': {
+		'en': 'port',
+		'de': 'port',
+		'ru': 'port',
+		'pt': 'port',
+		'nl': 'port',
+		'fr': 'port',
+		'it': 'port',
+		'es': 'port',
+		'pl': 'port',
+		'zh-cn': 'port'
+	},
+	'myColor': {
+		'en': 'myColor',
+		'de': 'meineColor',
+		'ru': 'Мой цвет',
+		'pt': 'minhaCor',
+		'nl': 'mijnKleur',
+		'fr': 'maCouleur',
+		'it': 'mioColore',
+		'es': 'miColor',
+		'pl': 'mójKolor',
+		'zh-cn': '我的颜色'
+	},
+	'myColor_tooltip': {
+		'en': 'Description of\nmyColor',
+		'de': 'Beschreibung von\nmyColor',
+		'ru': 'Описание\nmyColor',
+		'pt': 'Descrição de\nmyColor',
+		'nl': 'Beschrijving van\nmyColor',
+		'fr': 'Description de\nmyColor',
+		'it': 'Descrizione di\nmyColor',
+		'es': 'Descripción de\nmyColor',
+		'pl': 'Opis\nmyColor',
+		'zh-cn': '\nmyColor的描述'
+	},
+	'htmlText': {
+		'en': 'htmlText',
+		'de': 'htmlText',
+		'ru': 'htmlText',
+		'pt': 'htmlText',
+		'nl': 'htmlText',
+		'fr': 'htmlText',
+		'it': 'htmlText',
+		'es': 'htmlText',
+		'pl': 'htmlText',
+		'zh-cn': 'htmlText'
+	},
+	'group_extraMyset': {
+		'en': 'extraMyset',
+		'de': 'extraMyset',
+		'ru': 'extraMyset',
+		'pt': 'extraMyset',
+		'nl': 'extraMyset',
+		'fr': 'extraMyset',
+		'it': 'extraMyset',
+		'es': 'extraMyset',
+		'pl': 'extraMyset',
+		'zh-cn': 'extraMyset'
+	},
+	'extraAttr': {
+		'en': 'extraAttr',
+		'de': 'extraAttr',
+		'ru': 'extraAttr',
+		'pt': 'extraAttr',
+		'nl': 'extraAttr',
+		'fr': 'extraAttr',
+		'it': 'extraAttr',
+		'es': 'extraAttr',
+		'pl': 'extraAttr',
+		'zh-cn': 'extraAttr'
+	},
+	'Instance': {
+		'en': 'Instance',
+		'de': 'Instanz',
+		'ru': 'Инстанция',
+		'pt': 'Instância',
+		'nl': 'Instantie',
+		'fr': 'Instance',
+		'it': 'Esempio',
+		'es': 'Instancia',
+		'pl': 'Instancja',
+		'zh-cn': '例'
+	}
+};
